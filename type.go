9565411753655233a9e0ef743f5277792a7071d1
unknown <rouchashoori@gmail.com>
2024-07-10 14:15:08 +0330

    created interface with show message method for error and api response

--- type.go
+++ type.go
@@ -1,9 +1,54 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
 
+	"github.com/dustin/go-humanize"
+	"github.com/fatih/color"
+)
+
+type Response interface {
+	ShowMessage(string)
+}
 
 type APIResponse struct {
-	Price 	string	`json:"value"`
-	Change 	int32 	`json:"change"`
-	Date	string	`json:"date"`
+	Price  		string  `json:"value"`
+	Change 		float32 `json:"change"`
+	Date   		string  `json:"date"`
+}
+
+func (a *APIResponse) ShowMessage(currencyCode string) {
+	// convert price string to int64
+	priceInt , err := strconv.ParseInt(a.Price,10,64)
+
+	var PriceInHumanReadableFormat string
+	// make price in human readable format
+	if err != nil {
+		PriceInHumanReadableFormat = a.Price
+	} else {
+		PriceInHumanReadableFormat = humanize.Comma(priceInt)
+	}
+	// format a message
+	message := fmt.Sprintf("Price Of %s at %s - %sT with %.2fR of Changes." ,currencyCode, a.Date , PriceInHumanReadableFormat , a.Change)
+	
+	// if price goes up
+	if a.Change > 0 {
+		color.Blue("▲ %s" , message)
+	} else if a.Change < 0 { // if price goes down
+		color.Red("▼ %s" , message)
+	} else { // if price stays
+		color.White("- %s" , message)
+	}
+	
+}
+
+type Error struct {
+	Currency string
+	Code     int32
+	Date     string
+}
+
+func (e *Error) ShowMessage(currencyCode string) {
+	color.Red("- Price of %s you entered not found %d." , e.Currency, e.Code)
 }
