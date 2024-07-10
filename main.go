298287183b6384d5ee80c1c84155a13a0d69baac
unknown <rouchashoori@gmail.com>
2024-07-10 14:34:21 +0330

    added default values

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"os"
 )
 
 func main() {
+	args := []string{"btc","eth","usdt"}
+	if len(os.Args) > 1 {
+		args = os.Args[1:]
+		fmt.Println("If you do not enter any currency code the defaults are: BTC,ETH,USDT")
+	}
 
-	responses := IterateOverCurrencies(os.Args[1:])
 	
+	responses := IterateOverCurrencies(args)
+
 	for index, response := range responses {
 		response.ShowMessage(os.Args[index+1])
 	}
